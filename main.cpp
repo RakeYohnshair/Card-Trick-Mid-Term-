b18f0ed23b4b254982bee4b0005b7c0ba3572162
Jake J <31711821+RakeYohnshair@users.noreply.github.com>
2017-10-22 20:53:03 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,12 +12,28 @@ string cards[52] = {
       "A♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦","J♦","Q♦","K♦"};
 
 
-int main () {
-  srand ( time(NULL) );
-  for (int srand = 1; srand <= 21; srand++) {
-  int RandIndex = rand() % 22; //generates a random number between 0 and 21
-  cout << cards[RandIndex] << endl;
+  int randCards[21]; //array to hold the 21 random cards 
+int Pile1[7]; //Piles 1-3 for the 7 cards
+int Pile2[7];
+int Pile3[7];
+int combinedPiles[21]; //3 piles stacked back together
+int combinedPiles[11]; //answer
+
+int splitdeck () {
+ srand ( time(NULL) );
+  for (int srand = 1; srand <= 21; srand++) { //Tells it to pick 21 random cards from the array
+    int RandIndex = rand() % 52; //Tells it to pick from the 52 cards
+    cout << cards[RandIndex] << endl;
+      return 0;
   }
   cout << "Pick a card and remember it" ;
-    return 0;
+  
+    return randCards; // I have no idea what to return here
+}
+
+
+
+int main () {
+  cout << "xyz" << endl;
+
 }
