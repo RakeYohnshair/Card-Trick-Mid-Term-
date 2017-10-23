8e14b69efa1186abb3a22fec3e584bd778b8e443
Jake J <31711821+RakeYohnshair@users.noreply.github.com>
2017-10-22 20:16:33 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+string cards[52] = {
+      "A♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","10♥","J♥","Q♥","K♥",
+      "A♣","2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","10♣","J♣","Q♣","K♣",
+      "A♠","2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","10♠","J♠","Q♠","K♠",
+      "A♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦","J♦","Q♦","K♦"};
+
+
+int main () {
+  srand ( time(NULL) );
+  for (int srand = 1; srand <= 21; srand++) {
+  int RandIndex = rand() % 22; //generates a random number between 0 and 21
+  cout << cards[RandIndex] << endl;
+  }
+  cout << "Pick a card and remember it" ;
+    return srand;
+}
